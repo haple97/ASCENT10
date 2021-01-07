b022c9572aca549c712e531b18c67de3e9b21358
haple97 <62973070+haple97@users.noreply.github.com>
2021-01-07 11:38:57 -0500

    Add files via upload

---
+++ FLEET_Refresh/OD Matrix Generation Code/demand_matrix_conversions.m
@@ -0,0 +1,55 @@
+clc
+clear all 
+
+%demand matrix yearly
+demand_matrix_2005_yearly = readtable('demand_matrix_2005_yearly');
+save('demand_matrix_2005_yearly.mat','demand_matrix_2005_yearly');
+
+%demand matrix Jan
+demand_matrix_2005_Jan = readtable('demand_matrix_2005_Jan');
+save('demand_matrix_2005_Jan.mat','demand_matrix_2005_Jan');
+
+%demand matrix Feb
+demand_matrix_2005_Feb = readtable('demand_matrix_2005_Feb');
+save('demand_matrix_2005_Feb.mat','demand_matrix_2005_Feb');
+
+%demand matrix Mar
+demand_matrix_2005_Mar = readtable('demand_matrix_2005_Mar');
+save('demand_matrix_2005_Mar.mat','demand_matrix_2005_Mar');
+
+
+%demand matrix Apr
+demand_matrix_2005_Apr = readtable('demand_matrix_2005_Apr');
+save('demand_matrix_2005_Apr.mat','demand_matrix_2005_Apr');
+
+%demand matrix May
+demand_matrix_2005_May = readtable('demand_matrix_2005_May');
+save('demand_matrix_2005_May.mat','demand_matrix_2005_May');
+
+%demand matrix Jun
+demand_matrix_2005_Jun = readtable('demand_matrix_2005_Jun');
+save('demand_matrix_2005_Jun.mat','demand_matrix_2005_Jun');
+
+%demand matrix Jul
+demand_matrix_2005_Jul = readtable('demand_matrix_2005_Jul');
+save('demand_matrix_2005_Jul.mat','demand_matrix_2005_Jul');
+
+%demand matrix Aug
+demand_matrix_2005_Aug = readtable('demand_matrix_2005_Aug');
+save('demand_matrix_2005_Aug.mat','demand_matrix_2005_Aug');
+
+%demand matrix Sep
+demand_matrix_2005_Sep = readtable('demand_matrix_2005_Sep');
+save('demand_matrix_2005_Sep.mat','demand_matrix_2005_Sep');
+
+%demand matrix Oct
+demand_matrix_2005_Oct = readtable('demand_matrix_2005_Oct');
+save('demand_matrix_2005_Oct.mat','demand_matrix_2005_Oct');
+
+%demand matrix Nov
+demand_matrix_2005_Nov = readtable('demand_matrix_2005_Nov');
+save('demand_matrix_2005_Nov.mat','demand_matrix_2005_Nov');
+
+%demand matrix Dec
+demand_matrix_2005_Dec = readtable('demand_matrix_2005_Dec');
+save('demand_matrix_2005_Dec.mat','demand_matrix_2005_Dec');
